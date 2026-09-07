859427bf38245a27a3c022567d9d95e746ba9d44
Aditech <aditech-commits@github.com>
2026-09-07 20:44:53 +0100

    fix: replace logo font text with high-resolution transparent Butler brand logo image assets to match exact reference screenshot

--- header.php
+++ header.php
@@ -177,46 +177,7 @@
           <?php else : ?>
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/logo.png" alt="<?php bloginfo('name'); ?>" class="h-10 md:h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105" />
           <?php endif; ?>
-          <span class="font-brand-logo logo-text site-title flex items-center group-hover:text-accent-terracotta transition-colors">
-            <svg viewBox="0 0 395 38" class="h-6 md:h-8 w-auto text-cacao-dark group-hover:text-accent-terracotta fill-current transition-colors" aria-label="EVERYTHING CACAO">
-              <g fill="currentColor">
-                <!-- E -->
-                <path d="M8 8h17v3.5H12.5v7H23v3.5h-10.5v8H25.5V33.5H8V8z"/>
-                <!-- V -->
-                <path d="M27.5 8h4.2l6.8 19.2L45.3 8h4.2l-9.3 25.5h-3.4L27.5 8z"/>
-                <!-- E -->
-                <path d="M51 8h17v3.5H55.5v7H66v3.5h-10.5v8H69.5V33.5H51V8z"/>
-                <!-- R with sweeping swash tail under Y -->
-                <path d="M71.5 8h12c5.2 0 8.8 2.8 8.8 7.2 0 3.8-2.6 6.1-6.2 6.9 2.5 0.8 4.2 2.2 4.2 4.5 0 2.8-2.1 4.5-5.5 4.5h-2.1c2.8 0 4.2-1.2 4.2-3.1 0-2.2-2.1-3.6-6.2-3.6h-4.7v9.1H71.5V8zm4.5 3.5v8.5h7c3 0 5-1.5 5-4.25s-2-4.25-5-4.25h-7z"/>
-                <path d="M85.5 22.8c6.2 0 11.5 2.8 17.2 4.8 5.8 2 11.8 3.4 18.3 3.4 4.5 0 8.5-.8 11.5-2.2v2.1c-3.2 1.6-7.5 2.5-12.2 2.5-7 0-13.2-1.5-19.2-3.6-5.2-1.8-10.2-4.2-15.6-4.2-2.5 0-4.5.6-6.2 1.5v-2.8c2-1 4.2-1.5 6.2-1.5z"/>
-                <!-- Y -->
-                <path d="M97 8h4.5l7 11.5L115.5 8h4.5l-9.5 14.8v10.7H106.5V22.8L97 8z"/>
-                <!-- T -->
-                <path d="M120 8h20v3.5h-8v22H127.5v-22H120V8z"/>
-                <!-- H -->
-                <path d="M142 8h4.5v11h12V8h4.5v25.5h-4.5V22.5h-12v11H142V8z"/>
-                <!-- I -->
-                <path d="M165 8h4.5v25.5H165V8z"/>
-                <!-- N -->
-                <path d="M172.5 8h4.2l13.8 19.5V8h4.2v25.5h-3.8L176.7 13.5v20H172.5V8z"/>
-                <!-- G -->
-                <path d="M200 20.8c0-7.8 5.8-13.3 14-13.3 5.5 0 9.8 2.4 12.2 6.5l-3.2 2.1c-1.8-3.2-5.2-5.1-9-5.1-6 0-10 4.2-10 9.8s4 9.8 10 9.8c3.8 0 7.2-2 9.2-5.2h-7.5v-3.5h11.8v11.2c-3.2 4.2-8.2 6.5-13.5 6.5-8.2 0-14-5.5-14-13.3z"/>
-                <!-- C -->
-                <path d="M242 20.8c0-7.8 5.8-13.3 14-13.3 6.2 0 10.8 3.5 12.5 8.8l-3.5 1.5c-1.4-4.2-5.1-6.8-9-6.8-6 0-10 4.2-10 9.8s4 9.8 10 9.8c4 0 7.8-2.8 9.2-7l3.5 1.4c-1.8 5.5-6.5 9.1-12.7 9.1-8.2 0-14-5.5-14-13.3z"/>
-                <!-- A -->
-                <path d="M275 8h3.8l9.8 25.5h-4.2l-2.6-7h-9.8l-2.6 7h-4.2L275 8zm4.2 15l-3.3-8.8-3.3 8.8h6.6z"/>
-                <!-- C -->
-                <path d="M291 20.8c0-7.8 5.8-13.3 14-13.3 6.2 0 10.8 3.5 12.5 8.8l-3.5 1.5c-1.4-4.2-5.1-6.8-9-6.8-6 0-10 4.2-10 9.8s4 9.8 10 9.8c4 0 7.8-2.8 9.2-7l3.5 1.4c-1.8 5.5-6.5 9.1-12.7 9.1-8.2 0-14-5.5-14-13.3z"/>
-                <!-- A -->
-                <path d="M324 8h3.8l9.8 25.5h-4.2l-2.6-7h-9.8l-2.6 7h-4.2L324 8zm4.2 15l-3.3-8.8-3.3 8.8h6.6z"/>
-                <!-- O with Leaf Counter Motif -->
-                <path d="M340 20.8c0-7.8 6.2-13.3 14.5-13.3S369 13 369 20.8s-6.2 13.3-14.5 13.3S340 28.6 340 20.8zm24.5 0c0-6-3.8-9.8-10-9.8s-10 3.8-10 9.8 3.8 9.8 10 9.8 10-3.8 10-9.8z"/>
-                <!-- Leaf inside O counter -->
-                <path d="M354.5 15.5c2.5 2 3.8 5.2 2.8 8.5-1 3.2-3.8 5.5-7 5.8 2.2-2.2 3.2-5.2 2.5-8.2-.8-3-3.2-5.2-6-5.8 2.8-.5 5.8 0 7.7-0.3z"/>
-                <path d="M347.2 25.2c2.2-2.5 4.5-5.2 6.8-7.8" stroke="currentColor" stroke-width="0.8" fill="none"/>
-              </g>
-            </svg>
-          </span>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brand/logo-text-butler.png" alt="EVERYTHING CACAO" class="h-6 md:h-8 w-auto object-contain logo-text site-title font-brand-logo transition-transform duration-300 group-hover:scale-105" />
         </a>
       </div>
 
